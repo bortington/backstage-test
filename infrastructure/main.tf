184fa0918598437ad2dd9d31542f0f748edf6ba9
Alek Ivankovic <alek.ivankovic@rac.com.au>
2022-08-05 18:17:51 +0800

    Removed backend

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,13 +5,17 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "rg-common"
-    storage_account_name = "badbortcommontfstatesta"
-    container_name       = "sc-backstage-terraform"
-    key                  = "backstage-infrastructure.tfstate"
-    subscription_id      = "bd8e250a-66a6-4038-acd8-0d6aced3e3c8"
+  terraform {
+    backend "remote" {}
   }
+
+  # backend "azurerm" {
+  #   resource_group_name  = "rg-common"
+  #   storage_account_name = "badbortcommontfstatesta"
+  #   container_name       = "sc-backstage-terraform"
+  #   key                  = "backstage-infrastructure.tfstate"
+  #   subscription_id      = "bd8e250a-66a6-4038-acd8-0d6aced3e3c8"
+  # }
 }
 
 provider "azurerm" {
@@ -40,4 +44,4 @@ resource "azurerm_storage_container" "terraform_state" {
   name                  = "sc-backstage-terraform"
   container_access_type = "private"
   storage_account_name  = azurerm_storage_account.terraform_state_storage.name
-}
+} 
