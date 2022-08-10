db527bd82acd8d27c40bd7d4d74fa8d924d6c2d4
Alek Ivankovic <alek.ivankovic@rac.com.au>
2022-08-10 12:30:27 +0800

    Removed old backend resources and using new storage container

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,7 +10,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-common"
     storage_account_name = "badbortcommontfstatesta"
-    container_name       = "sc-backstage-terraform"
+    container_name       = "backstage-test"
     key                  = "backstage-infrastructure.tfstate"
     subscription_id      = "bd8e250a-66a6-4038-acd8-0d6aced3e3c8"
   }
@@ -28,18 +28,3 @@ resource "azurerm_resource_group" "rg_backstage" {
   location = "Australia East"
   tags     = var.resource_tags
 }
-
-resource "azurerm_storage_account" "terraform_state_storage" {
-  name                     = "bertstabackstage"
-  account_replication_type = "LRS"
-  account_tier             = "Standard"
-  min_tls_version          = "TLS1_2"
-  location                 = azurerm_resource_group.rg_backstage.location
-  resource_group_name      = azurerm_resource_group.rg_backstage.name
-}
-
-resource "azurerm_storage_container" "terraform_state" {
-  name                  = "sc-backstage-terraform"
-  container_access_type = "private"
-  storage_account_name  = azurerm_storage_account.terraform_state_storage.name
-} 
