c1e03d732c9325a04453af566454f98702bdb2db
Alek Ivankovic <alek.ivankovic@rac.com.au>
2022-08-10 13:48:57 +0800

    Tweaking aci props

--- infrastructure/containers.tf
+++ infrastructure/containers.tf
@@ -8,14 +8,14 @@ data "azurerm_dns_zone" "badbort" {
 }
 
 resource "azurerm_container_group" "cg" {
-  name                = "mycontainer"
+  name                = "aci-backstage"
   location            = azurerm_resource_group.rg_backstage.location
   resource_group_name = azurerm_resource_group.rg_backstage.name
   ip_address_type     = "Public"
-  dns_name_label      = "bortacusmaximus"
+  dns_name_label      = "backstage"
   os_type             = "Linux"
 
-  container {
+    container {
     name   = "testsite"
     image  = "registry.hub.docker.com/bortos/fredstage:latest"
     cpu    = "0.5"
