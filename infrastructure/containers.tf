74e9a4879f035c08d49c499e84d084c73510079d
Alek Ivankovic <alek.ivankovic@rac.com.au>
2022-08-10 13:19:37 +0800

    Fixing resource grp

--- infrastructure/containers.tf
+++ infrastructure/containers.tf
@@ -31,7 +31,7 @@ resource "azurerm_container_group" "cg" {
 resource "azurerm_dns_cname_record" "example" {
   name                = "www"
   zone_name           = data.azurerm_dns_zone.badbort.name
-  resource_group_name = azurerm_resource_group.rg_backstage.name
+  resource_group_name = data.azurerm_dns_zone.badbort.resource_group_name
   ttl                 = 300
   record              = azurerm_container_group.cg.fqdn
 }
