95f16401ee081454609bbb46831ccf3fb4cbfb88
Alek Ivankovic <alek.ivankovic@rac.com.au>
2022-08-15 17:18:14 +0800

    Attempting to use caddy

--- infrastructure/containers.tf
+++ infrastructure/containers.tf
@@ -1,7 +1,3 @@
-# data "docker_registry_image" "ubuntu" {
-#   name = "ubuntu:latest"
-# }
-
 data "azurerm_dns_zone" "badbort" {
   name                = "badbort.com"
   resource_group_name = "rg-common"
@@ -19,18 +15,45 @@ resource "azurerm_container_group" "cg" {
     name   = "backstage"
     image  = "registry.hub.docker.com/bortos/fredstage:latest"
     cpu    = "0.5"
-    memory = "1"
+    memory = "0.5"
 
     ports {
       port     = 7007
       protocol = "TCP"
     }
-
+    
     secure_environment_variables = {
       GITHUB_TOKEN              = var.GITHUB_TOKEN
       AUTH_GITHUB_CLIENT_ID     = var.AUTH_GITHUB_CLIENT_ID
       AUTH_GITHUB_CLIENT_SECRET = var.AUTH_GITHUB_CLIENT_SECRET
     }
+  }
+
+  container {
+    name   = "caddy"
+    image  = "caddy"
+    cpu    = "0.5"
+    memory = "0.5"
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+
+    ports {
+      port     = 443
+      protocol = "TCP"
+    }
+
+    volume {
+      name = "aci-caddy-data"
+      mount_path = "/data"
+      storage_account_name = azurerm_storage_account.sta.name
+      storage_account_key = azurerm_storage_account.sta.primary_access_key
+      share_name = azurerm_storage_share.caddy_sc.name
+    }
+
+    commands = [ "caddy", "reverse-proxy", "--from", "backstage.australiaeast.azurecontainer.io","--to", "localhost:7007" ]
   }
 }
 
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -28,3 +28,18 @@ resource "azurerm_resource_group" "rg_backstage" {
   location = "Australia East"
   tags     = var.resource_tags
 }
+
+resource "azurerm_storage_account" "sta" {
+  name = "backstage-testing-sta"
+  account_kind = "Standard"
+  account_replication_type = "LRS"
+  enable_https_traffic_only = true
+  resource_group_name = azurerm_resource_group.rg_backstage.name
+  location = azurerm_resource_group.rg_backstage.location
+}
+
+resource "azurerm_storage_share" "caddy_sc" {
+  name = "backstage-caddy-data"
+  storage_account_name = azurerm_storage_account.sta.name
+  quota = 1
+}
